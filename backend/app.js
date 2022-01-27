58c249aecb6296851bbb4891ed4334f2b1d0c47f
Ilya Dykin <dddiii06@yandex.ru>
2022-01-27 21:09:44 +0300

    cors1

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ const NotFoundError = require("./errors/not-found-error");
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
