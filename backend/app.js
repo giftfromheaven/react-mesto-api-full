36b608599404e2399fc1c5d0ba954d0db18dec6c
Ilya Dykin <dddiii06@yandex.ru>
2022-01-28 19:52:21 +0300

    cors2

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ const allowedCors = [
 app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", true);
   }
   const { method } = req;
